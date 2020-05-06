73344852606edb86b75be53099dc76e92e54e2d3
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-05-06 12:31:50 +0100

    Creating connection between student and course in database

---
+++ hb-05-many-to-many/src/com/luv2code/hibernate/demo/AddCoursesForMaryDemo.java
@@ -0,0 +1,65 @@
+package com.luv2code.hibernate.demo;
+
+
+import com.luv2code.hibernate.demo.entity.*;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class AddCoursesForMaryDemo {
+
+    public static void main(String[] args) {
+
+        // create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+
+        // create session
+        Session session = factory.getCurrentSession();
+
+        try {
+
+            // start a transaction
+            session.beginTransaction();
+
+            // get the student marry from Database
+            int theId = 2;
+
+            Student tempStudent = session.get(Student.class, theId);
+
+            System.out.println("\nLoaded student " + tempStudent);
+            System.out.println("courses " + tempStudent.getCourses());
+
+            // create more courses
+            Course tempCourse1 = new Course("Rubik's Cube - How to Speed Cube");
+            Course tempCourse2 = new Course("Atari 2600 - Game Development");
+
+            // add student to courses
+            tempCourse1.addStudent(tempStudent);
+            tempCourse2.addStudent(tempStudent);
+
+            // save the courses
+            System.out.println("\nSaving the courses...");
+
+            session.save(tempCourse1);
+            session.save(tempCourse2);
+
+            // commit the transaction
+            session.getTransaction().commit();
+
+            System.out.println("Done!");
+
+
+        } finally {
+            session.close();
+            factory.close();
+        }
+
+    }
+}
