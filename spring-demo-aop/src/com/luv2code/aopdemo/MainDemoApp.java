9dc7e4b13887be3ec7c4724c744a9c26826cc878
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-06-04 12:03:41 +0100

    Pointcut Expressions - Match any add* Method

--- spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -1,6 +1,8 @@
 package com.luv2code.aopdemo;
 
 import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.aspectj.weaver.Member;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MainDemoApp {
@@ -13,14 +15,14 @@ public class MainDemoApp {
         // get the bean from the bean container
         AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
 
-        // call the business methods
+        // get membership bean from the bean container
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        // call the business method
         theAccountDAO.addAccount();
 
-        // do it again!
-        System.out.println("\n let's call it again! \n");
-
-        // call the business method again
-        theAccountDAO.addAccount();
+        // call the membership business method
+        theMembershipDAO.addSillyMember();
 
         // close the context
         context.close();
