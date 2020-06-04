44ed9b17693918afb7674d7664c682baf255a709
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-06-04 16:05:07 +0100

    AOP: Combining Pointcuts

---
+++ spring-demo-aop-poincut-declarations-combo/src/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,40 @@
+package com.luv2code.aopdemo;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MainDemoApp {
+
+    public static void main(String[] args) {
+
+        // read spring configuration java class
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+
+        // get the bean from the bean container
+        AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+
+        // get membership bean from the bean container
+        MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+
+        // call the business method
+        Account theAccount = new Account();
+        theAccountDAO.addAccount(theAccount, true);
+        theAccountDAO.doWork();
+
+        // call the accountDao getter/setter methods
+
+        theAccountDAO.setName("foobar");
+        theAccountDAO.setServiceName("silver");
+        theAccountDAO.getName();
+        theAccountDAO.getServiceName();
+
+        // call the membership business method
+        theMembershipDAO.addSillyMember();
+        theMembershipDAO.goToSleep();
+
+        // close the context
+        context.close();
+
+    }
+}
