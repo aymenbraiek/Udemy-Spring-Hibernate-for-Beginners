add46452dcb6e168a659d7a3e785e731b483adaf
Szpytma <56477986+Szpytma@users.noreply.github.com>
2020-06-04 10:33:11 +0100

    AOP: Pointcut Expressions - Match Methods and Return Types

--- spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -12,8 +12,7 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-   // @Before("execution(public void addAccount())")
-    @Before("execution(public void updateAccount())")
+    @Before("execution(public void addAccount())")
     public void beforeAddAccountAdvice() {
 
         System.out.println("\n=====>>>> Executing @Before advice on method()");
