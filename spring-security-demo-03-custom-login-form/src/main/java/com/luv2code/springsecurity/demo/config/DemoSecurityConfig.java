60de33cb787119c19ddbf9c5c8649a4a1f2dd497
szpytma <szpytmapawel@hotmail.com>
2020-06-26 10:51:38 +0100

    Adding Custom Login Form

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/config/DemoSecurityConfig.java
@@ -19,11 +19,24 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
         UserBuilder users = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
-                .withUser(users.username("john").password("test123").roles("EMPLOYEE"))
-                .withUser(users.username("mary").password("test123").roles("MANAGER"))
+                .withUser(users.username("john").password("test123").roles("EMPLOYEE"));
+        auth.inMemoryAuthentication()
+                .withUser(users.username("mary").password("test123").roles("MANAGER"));
+        auth.inMemoryAuthentication()
                 .withUser(users.username("susan").password("test123").roles("ADMIN"));
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/showMyLoginPage")
+                .loginProcessingUrl("/authenticateTheUser")
+                .permitAll();
+    }
 }
 
 
