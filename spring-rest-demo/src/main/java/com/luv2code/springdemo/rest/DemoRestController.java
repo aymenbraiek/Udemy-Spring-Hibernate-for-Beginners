aa2fffbd60aae4394756c648c533724ec6063e98
szpytma <szpytmapawel@hotmail.com>
2020-07-03 13:40:36 +0100

    Creating rest controller

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/DemoRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/DemoRestController.java
@@ -1,4 +1,4 @@
-package com.luv2code.springdemo.contoller;
+package com.luv2code.springdemo.rest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
