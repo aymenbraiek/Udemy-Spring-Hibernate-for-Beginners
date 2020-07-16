38da6edafa84eadd932b13ac96316b3d1c2ee019
szpytma <szpytmapawel@hotmail.com>
2020-07-16 13:08:34 +0100

    REST API with JPA DAO Implementation

---
+++ 22-jpa-crud-demo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOJpaImpl.java
@@ -0,0 +1,53 @@
+package com.luv2code.springboot.cruddemo.dao;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+@Repository
+public class EmployeeDAOJpaImpl implements EmployeeDAO {
+
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public EmployeeDAOJpaImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    public List<Employee> findAll() {
+
+        // create a query
+        Query theQuery = entityManager.createQuery("from Employee");
+
+        // execute query and get result list & return list
+        return theQuery.getResultList();
+    }
+
+    @Override
+    public Employee findById(int theId) {
+
+        // get employee & return employee
+        return entityManager.find(Employee.class, theId);
+    }
+
+    @Override
+    public void save(Employee theEmployee) {
+        // save or update the employee
+        Employee dbEmployee = entityManager.merge(theEmployee);
+        // update with id form db ... so we can get generated if for save/insert
+        theEmployee.setId(dbEmployee.getId());
+    }
+
+    @Override
+    public void deleteById(int theId) {
+        Query theQuery = entityManager.createQuery("delete from Employee where id=:employeeId");
+        theQuery.setParameter("employeeId", theId);
+        theQuery.executeUpdate();
+    }
+}
