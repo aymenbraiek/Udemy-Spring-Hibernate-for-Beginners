7b9a118dd53daf6de0b54426f7db7b9db0e47a54
szpytma <szpytmapawel@hotmail.com>
2020-08-18 16:53:02 +0100

    Spring boot th - Crud database

---
+++ 35-thymeleafdemo-employees-add/src/main/java/com/luv2code/springboot/thymeleafdemo/service/EmployeeService.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.service;
+
+
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    List<Employee> findAll();
+
+    Employee findById(int theId);
+
+    void save(Employee theEmployee);
+
+    void deleteById(int theId);
+}
